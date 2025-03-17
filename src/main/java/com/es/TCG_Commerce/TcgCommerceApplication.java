b375f9df5f15a6969aff73010a25529c56d278ef
L <srleon57@gmail.com>
2025-03-17 05:20:55 +0100

    retoques Y VA!!!

--- src/main/java/com/es/TCG_Commerce/TcgCommerceApplication.java
+++ src/main/java/com/es/TCG_Commerce/TcgCommerceApplication.java
@@ -1,9 +1,14 @@
 package com.es.TCG_Commerce;
 
+import com.es.TCG_Commerce.security.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@EnableAsync
 public class TcgCommerceApplication {
 
 	public static void main(String[] args) {
