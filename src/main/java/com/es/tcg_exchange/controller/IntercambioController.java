dc2aca46696e21a11d1e11b7ce44b5611bc3ec60
L <srleon57@gmail.com>
2026-02-05 14:35:31 +0100

    intercambio controller

--- src/main/java/com/es/tcg_exchange/controller/IntercambioController.java
+++ src/main/java/com/es/tcg_exchange/controller/IntercambioController.java
@@ -7,45 +7,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/transacciones")
+@RequestMapping("/intercambios")
 public class IntercambioController {
 
     @Autowired
-    private IntercambioService tservice;
+    private IntercambioService intercambioService;
 
-    // no tiene sentido obtener todas las transacciones, no lo quiero para mi API
-//    @GetMapping("/")
-//    public ResponseEntity<List<TransaccionDTO>> getAll(){
-//        List<TransaccionDTO> tdto = tservice.getAll();
-//
-//        return new ResponseEntity<List<TransaccionDTO>>(tdto, HttpStatus.OK);
-//    }
+    // Obtener intercambios de un usuario
+    @GetMapping("/usuario/{username}")
+    public ResponseEntity<List<IntercambioDTO>> getIntercambiosDeUsuario(@PathVariable String username,
+                                                                         @RequestParam(required = false) String estado) {
+        List<IntercambioDTO> intercambiosDTO = intercambioService.findByUsuario(username, estado);
+        return new ResponseEntity<>(intercambiosDTO, HttpStatus.OK);
+    }
 
+    // Obtener intercambio por id
     @GetMapping("/{id}")
-    public ResponseEntity<IntercambioDTO> findById(@PathVariable Long id){
-        IntercambioDTO tdto = tservice.findById(id);
-
-        return new ResponseEntity<IntercambioDTO>(tdto, HttpStatus.OK);
+    public ResponseEntity<IntercambioDTO> getIntercambioById(@PathVariable Long id) {
+        IntercambioDTO intercambioDTO = intercambioService.findById(id);
+        return new ResponseEntity<>(intercambioDTO, HttpStatus.OK);
     }
 
+    // Crear nuevo intercambio
     @PostMapping
-    public ResponseEntity<IntercambioDTO> insert(@RequestBody IntercambioDTO tdto){
-        tservice.insert(tdto);
-
-        return new ResponseEntity<IntercambioDTO>(tdto, HttpStatus.OK);
+    public ResponseEntity<IntercambioDTO> createIntercambio(@RequestBody IntercambioDTO intercambioDTO) {
+        intercambioService.insert(intercambioDTO);
+        return new ResponseEntity<>(intercambioDTO, HttpStatus.CREATED);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<IntercambioDTO> update(@PathVariable Long id, @RequestBody IntercambioDTO tdto){
-        tservice.update(id, tdto);
-
-        return new ResponseEntity<IntercambioDTO>(tdto, HttpStatus.OK);
+    // Aceptar intercambio
+    @PutMapping("/{id}/aceptar")
+    public ResponseEntity<IntercambioDTO> aceptarIntercambio(@PathVariable Long id) {
+        IntercambioDTO intercambioDTO = intercambioService.aceptar(id);
+        return new ResponseEntity<>(intercambioDTO, HttpStatus.OK);
     }
 
+    // Rechazar intercambio
+    @PutMapping("/{id}/rechazar")
+    public ResponseEntity<IntercambioDTO> rechazarIntercambio(@PathVariable Long id) {
+        IntercambioDTO intercambioDTO = intercambioService.rechazar(id);
+        return new ResponseEntity<>(intercambioDTO, HttpStatus.OK);
+    }
+
+    // Eliminar intercambio
     @DeleteMapping("/{id}")
-    public ResponseEntity<IntercambioDTO> delete(@PathVariable Long id){
-
-        return new ResponseEntity<IntercambioDTO>(tservice.delete(id), HttpStatus.OK);
+    public ResponseEntity<IntercambioDTO> deleteIntercambio(@PathVariable Long id) {
+        IntercambioDTO intercambioDTO = intercambioService.delete(id);
+        return new ResponseEntity<>(intercambioDTO, HttpStatus.OK);
     }
 }
